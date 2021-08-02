22737ab252e1ae3866ca4cb32c35192e9ff39b63
elenaniksy <elfhelen@gmail.com>
2021-08-02 12:20:30 +0300

    feat: add base scss style to the application

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './app.scss';
 
-const App = () => {
+const App: React.FC = () => {
   return (
-    <div>
+    <div className={'app__wrapper'}>
+      {/* todo: implement here 2 components: search bar and card holder with cards in AUX wrapper*/}
       <h1 className={'header'}>My React and TypeScript App!</h1>
       <p className={'date'}>{new Date().toLocaleDateString()}</p>
     </div>
