68d8493842d03a24b891284fea6ed012e3046f9c
elenaniksy <elfhelen@gmail.com>
2021-08-02 12:56:46 +0300

    feat: add base styles to app component

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -5,8 +5,7 @@ const App: React.FC = () => {
   return (
     <div className={'app__wrapper'}>
       {/* todo: implement here 2 components: search bar and card holder with cards in AUX wrapper*/}
-      <h1 className={'header'}>My React and TypeScript App!</h1>
-      <p className={'date'}>{new Date().toLocaleDateString()}</p>
+      <h1 className={'app__header'}>TASK I: &lt;React Components&gt;</h1>
     </div>
   );
 };
