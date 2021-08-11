b8c6b884fe14a84a6238838b250c446f10443514
elenaniksy <elfhelen@gmail.com>
2021-08-11 20:10:10 +0300

    fix: fixed styles

--- react-forms/src/components/UI/button/Button.tsx
+++ react-forms/src/components/UI/button/Button.tsx
@@ -4,13 +4,14 @@ import classes from './Button.module.scss';
 type propsType = {
   type: string;
   children: string;
+  //todo: fix onclick type
   onClick: any;
   disabled?: boolean;
 };
 
 const Button: React.FC<propsType> = (props: propsType) => {
-  // @ts-ignore
-  const cls: string[] = [classes.Button, classes[props.type]];
+  // @ts-ignore //todo: fix classes[props.type] type
+  const cls: string[] = [classes.button, classes[props.type]];
 
   return (
     <button onClick={props.onClick} className={cls.join(' ')} disabled={props.disabled}>
