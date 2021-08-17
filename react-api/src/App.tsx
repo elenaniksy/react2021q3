0c035938470087d93e09815b1c4006b816cf7bd9
elenaniksy <elfhelen@gmail.com>
2021-08-17 13:15:19 +0300

    feat: add index.tsx, app component, add basic styles config of the application

---
+++ react-api/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './App.module.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className={classes.app}>
+      <h1>TASK III: &lt;React API&gt;</h1>
+      {/*//todo: form with search && articles holder -> render article*/}
+    </div>
+  );
+};
+
+export default App;
---
+++ react-api/src/interfaces/IClasses.tsx
@@ -0,0 +1,3 @@
+export interface IClasses {
+  [key: string]: string;
+}
