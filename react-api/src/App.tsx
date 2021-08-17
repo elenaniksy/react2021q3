75ad9607d8b0b5a89b625c50736349e91870c122
elenaniksy <elfhelen@gmail.com>
2021-08-17 14:42:00 +0300

    feat: import Form component

--- react-api/src/App.tsx
+++ react-api/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import classes from './App.module.scss';
+import Form from './compoments/form/Form';
 
 const App: React.FC = () => {
   return (
     <div className={classes.app}>
       <h1>TASK III: &lt;React API&gt;</h1>
       {/*//todo: form with search && articles holder -> render article*/}
+      <Form />
     </div>
   );
 };
