5d46e3d536514df2a0ea6f0e4b717e5493f818f6
elenaniksy <elfhelen@gmail.com>
2021-08-18 11:51:33 +0300

    feat: implement request with data gap

--- react-api/src/compoments/form/Form.tsx
+++ react-api/src/compoments/form/Form.tsx
@@ -10,16 +10,27 @@ import { SortType } from '../../interfaces/SortType';
 const API_KEY = 'ed028494cd0a467c9e2ac37f12bc2df4';
 
 const Form: React.FC = (): JSX.Element => {
-  const htmlFor = `search--${Math.random()}`;
   const [searchValue, setSearchValue] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [arts, setArt] = useState<IArticle[]>([]);
   const [sortBy, setSortBy] = useState<SortType>(SortType.popularity);
+  const [dateFrom, setDateFrom] = useState<string>('');
+  const [dateTo, setDateTo] = useState<string>('');
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const target: HTMLInputElement = event.target as HTMLInputElement;
     const value: string = target.value;
-    setSearchValue(value);
+
+    switch (target.id) {
+      case 'search':
+        setSearchValue(value);
+        break;
+      case 'dateFrom':
+        setDateFrom(value);
+        break;
+      case 'dateTo':
+        setDateTo(value);
+    }
   };
 
   const handleSubmit = async (event: React.ChangeEvent<HTMLFormElement>): Promise<void> => {
@@ -27,7 +38,7 @@ const Form: React.FC = (): JSX.Element => {
     try {
       setIsLoading(true);
       const response: AxiosResponse<IData_GET200> = await axiosInst.get(
-        `v2/everything?q=${searchValue}&apiKey=${API_KEY}&sortBy=${sortBy}`,
+        `v2/everything?q=${searchValue}&apiKey=${API_KEY}&from=${dateFrom}&to=${dateTo}&sortBy=${sortBy}`,
       );
       setArt(response.data.articles);
     } catch (e) {
@@ -35,6 +46,8 @@ const Form: React.FC = (): JSX.Element => {
     } finally {
       setIsLoading(false);
       setSearchValue('');
+      setDateFrom('');
+      setDateTo('');
     }
   };
 
@@ -42,8 +55,8 @@ const Form: React.FC = (): JSX.Element => {
     <div className={classes.form}>
       <form onSubmit={handleSubmit}>
         <div className={classes.line}>
-          <label htmlFor={htmlFor}>
-            <input id={htmlFor} type='text' value={searchValue} onChange={handleChange} disabled={isLoading} />
+          <label htmlFor={'search'}>
+            <input id={'search'} type='text' value={searchValue} onChange={handleChange} disabled={isLoading} />
           </label>
           <button type='submit' disabled={isLoading}>
             {isLoading ? 'Loading...' : 'Search'}
@@ -77,6 +90,10 @@ const Form: React.FC = (): JSX.Element => {
             />
             publishedAt
           </label>
+        </div>
+        <div className={classes.line}>
+          <input id={'dateFrom'} type='date' value={dateFrom} onChange={handleChange} />
+          <input id={'dateTo'} type='date' value={dateTo} onChange={handleChange} />
         </div>
       </form>
       <ArticlesHolder articles={arts} />
