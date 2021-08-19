0dfd87e05a1f2e32da905f39366dbc388b341e88
elenaniksy <elfhelen@gmail.com>
2021-08-19 13:43:26 +0300

    feat: create About component and add basic styles to it

--- react-routing/src/App.tsx
+++ react-routing/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import classes from './App.module.scss';
 import { Route } from 'react-router-dom';
-import Form from './compoments/form/Form';
 import Header from './compoments/header/Header';
+import Form from './compoments/form/Form';
+import About from './compoments/about/About';
 
 const App: React.FC = () => {
   return (
@@ -10,7 +11,8 @@ const App: React.FC = () => {
       <Header />
       <h1>TASK IV: &lt;React Routing&gt;</h1>
 
-      <Route path='/' exact render={() => <Form />} />
+      <Route path='/' exact component={Form} />
+      <Route path='/about' exact component={About} />
     </div>
   );
 };
