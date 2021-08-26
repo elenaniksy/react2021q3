86de5390b2eddb59de7e0da68c7dbe48e09e7da7
elenaniksy <elfhelen@gmail.com>
2021-08-26 13:57:27 +0300

    init: start react-redux task, add redux to project dependencies

--- react-redux/src/index.tsx
+++ react-redux/src/index.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore();
 
 const application: JSX.Element = (
-  <BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </BrowserRouter>
+  </Provider>
+
 );
 
 ReactDOM.render(application, document.getElementById('root'));
